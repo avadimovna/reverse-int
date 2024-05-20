73f400b469b640ce7bc89e6cd6df0610c959a34c
avadimovna <anastasia.khotyan@gmail.com>
2024-05-20 11:33:36 +0300

    Created function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let nToString = n.toString();
+  if (nToString.startsWith('-')) {
+    nToString = nToString.slice(1);
+  }
+  let result = '';
+  for (i = 0; i <= nToString.length - 1; i++) {
+    result = nToString[i] + result;
+  }
+  return result;
 }
